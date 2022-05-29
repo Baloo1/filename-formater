ed90d0a74d185da32e43045b8aed06c70fb0156c
Johannes Mattsson <developer@mattsson.cloud>
2022-05-29 22:21:46 +0200

    Fix cleanup formatter

--- index.ts
+++ index.ts
@@ -13,20 +13,15 @@ const readFiles = async () => {
   await Promise.all(fs.readdirSync(imageDir).map(async (file) => {
     const pathToCurrentFile = `${absolutePath}${file}`;
     const stats = fs.statSync(pathToCurrentFile);
+
+    if (file == 'node_modules' || file == '.git') return;
     if (stats.isDirectory()) {
       // read all files in dir
       await Promise.all(fs.readdirSync(pathToCurrentFile).map(async (file) => {
-        // append dir to filename and read file stats
-        const pathToFile = `${pathToCurrentFile}\\${file}`;
+        const newFileName = await createNewFileName(pathToCurrentFile, file);
+        const pathToFile = filePath(pathToCurrentFile, file);
 
-        const newFileName = await createNewFileName(
-          file,
-          pathToCurrentFile,
-          fs.statSync(pathToFile),
-        );
-        console.log("newFileName:", newFileName);
-
-        renameFile(pathToFile, newFileName);
+        writeNewFileName(pathToFile, newFileName);
         count++;
       }));
     }
@@ -35,10 +30,13 @@ const readFiles = async () => {
   console.timeEnd("startRenaming");
 };
 
-const getDateFromEXIF = async (file, pathToCurrentFile) => {
-  console.log(`${pathToCurrentFile}\\${file}`)
-  const file2 = fs.readFileSync(`${pathToCurrentFile}\\${file}`)
-  let { DateTimeOriginal } = await exifr.parse(file2);
+const writeNewFileName = (oldPath, newPath) => {
+  fs.renameSync(oldPath, newPath);
+  console.log(`File renamed from ${oldPath} to ${newPath} successfully!`);
+};
+
+const getDateFromEXIF = async (filePath) => {
+  let { DateTimeOriginal } = await exifr.parse(filePath);
   return parseDateString(DateTimeOriginal);
 }
 
@@ -48,81 +46,84 @@ const getDateFromLastModified = (stats) => {
 
 const parseDateString = (date) => {
   const year = date.getFullYear();
-  // adding a leading 0, by slicing -2 we get the last 2 digits, so 020 becomes 20 and 02 stays 02.
-  // also months are 0 indexed
   const month = leadingZero(date.getMonth() + 1);
   const day = leadingZero(date.getDate());
   const hours = leadingZero(date.getHours());
   const minutes = leadingZero(date.getMinutes());
   const seconds = leadingZero(date.getSeconds());
 
-  return [year, month, day, hours, minutes, seconds];
+  return { year, month, day, hours, minutes, seconds };
 }
 
 const getDateFromFileName = (file) => {
   console.log(`${file} being reformated`);
 
   const year = file.slice(4, 8);
-  // adding a leading 0, by slicing -2 we get the last 2 digits, so 020 becomes 20 and 02 stays 02.
-  // also months are 0 indexed
   const month = file.slice(8, 10);
   const day = file.slice(10, 12);
   const hours = file.slice(13, 15);
   const minutes = file.slice(15, 17);
   const seconds = file.slice(17, 19);
 
-  return [year, month, day, hours, minutes, seconds];
+  return { year, month, day, hours, minutes, seconds };
 }
 
-const createNewFileName = async (file, pathToCurrentFile, stats) => {
-  let year;
-  let month;
-  let day;
-  let hours;
-  let minutes;
-  let seconds;
+const createNewFileName = async (pathToCurrentFile, file) => {
+  let date;
 
+  const pathToFile = filePath(pathToCurrentFile, file);
   if (formatMethod === 'exif') {
-    [year, month, day, hours, minutes, seconds] = await getDateFromEXIF(file, pathToCurrentFile)
+    date = await getDateFromEXIF(pathToFile)
   } else if (formatMethod == 'reformat') {
-    [year, month, day, hours, minutes, seconds] = getDateFromFileName(file);
+    date = getDateFromFileName(file);
   } else {
-    [year, month, day, hours, minutes, seconds] = getDateFromLastModified(stats)
+    const stats = fs.statSync(pathToFile)
+    date = getDateFromLastModified(stats)
   }
 
-
-  if (
-    day.length < 2 ||
-    month.length < 2 ||
-    hours.length < 2 ||
-    minutes.length < 2 ||
-    seconds.length < 2
-  ) {
-    throw new Error("To short date");
-  }
+  if (!validateDate(date)) return;
 
   var fileEnding = file.slice(-3);
   if (fileEnding.toUpperCase() === "PEG") {
     fileEnding = file.slice(-4);
   }
 
-  let newFileName = `${pathToCurrentFile}\\${year}-${month}-${day} ${hours}.${minutes}.${seconds}.${fileEnding}`;
+  let newFileName = `${fileNameFormat(pathToCurrentFile, date)}.${fileEnding}`;
 
   let fileNbr = 1;
   while (fs.existsSync(newFileName)) {
-    newFileName = `${pathToCurrentFile}\\${year}-${month}-${day} ${hours}.${minutes}.${seconds} (${fileNbr}).${fileEnding}`;
+    newFileName = `${fileNameFormat(pathToCurrentFile, date)} (${fileNbr}).${fileEnding}`;
     fileNbr++;
   }
   return newFileName;
 };
 
+const validateDate = ({ year, month, day, hours, minutes, seconds }) => {
+  if (
+    year.length < 4 ||
+    month.length < 2 ||
+    day.length < 2 ||
+    hours.length < 2 ||
+    minutes.length < 2 ||
+    seconds.length < 2
+  ) {
+    throw new Error("To short date");
+  }
+  return true;
+}
+
+const fileNameFormat = (pathToCurrentFile, { year, month, day, hours, minutes, seconds }) => {
+  return `${pathToCurrentFile}\\${year}-${month}-${day} ${hours}.${minutes}.${seconds}`
+}
+
+const filePath = (pathToCurrentFile, file) => {
+  return `${pathToCurrentFile}\\${file}`;
+}
+
+// Adding a leading 0, by slicing -2 we get the last 2 digits, so 020 becomes 20 and 02 stays 02.
+// also months are 0 indexed
 const leadingZero = (nbr) => {
   return "0".concat(nbr).slice(-2);
-};
-
-const renameFile = (oldPath, newPath) => {
-  fs.renameSync(oldPath, newPath);
-  console.log(`File renamed from ${oldPath} to ${newPath} successfully!`);
 };
 
 readFiles();
