5f5793c5ac49d9710fa094ad3083201d9907569f
Johannes Mattsson <developer@mattsson.cloud>
2022-05-29 21:56:03 +0200

    Add exif parsing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Filename-formater
+# Image name formatter
 
-Renames files based on their timestamp for last modified to `YYYY-MM-DD hh.mm.ss` keeping the fileextension. Can also translate IMG filenames to the same format.
+Renames files based on their timestamp for last modified to `YYYY-MM-DD hh.mm.ss` keeping the file extension. Can also translate IMG filenames to the same format.
 
-Run with `npm start --REPLACE_WITH_DIRECTORY_PATH` where `REPLACE_WITH_DIRECTORY_PATH` can be relative or absolute. `npm start -- ./images`. Use `npm start -- ./images true` for translating from IMG. Both looks into 1 folder depth and formates everything except folders in 0 depth and everything in 1 depth.
+Run with `npm start --REPLACE_WITH_DIRECTORY_PATH` where `REPLACE_WITH_DIRECTORY_PATH` can be relative or absolute. `npm start -- ./images`. Use `npm start -- ./images exif` to format from EXIF data. For files with IMG start use  `npm start -- ./images reformat` for translating from IMG. Both looks into 1 folder depth and formats everything except folders in 0 depth and everything in 1 depth.
--- index.ts
+++ index.ts
@@ -1,40 +1,80 @@
 const fs = require("fs");
 const path = require("path");
+var exifr = require('exifr')
 const imageDir = process.argv[2];
-const reformatFileName = process.argv[3] || false;
+const formatMethod = process.argv[3] || false;
 var count = 0;
 
 console.time("startRenaming");
 const absolutePath = path.join(__dirname, "../", imageDir).concat("\\");
 
 // read image directory
-const readFiles = () => {
-  fs.readdirSync(imageDir).map((file) => {
-    const pathToCurrentFile = `${absolutePath}\\${file}`;
+const readFiles = async () => {
+  await Promise.all(fs.readdirSync(imageDir).map(async (file) => {
+    const pathToCurrentFile = `${absolutePath}${file}`;
     const stats = fs.statSync(pathToCurrentFile);
     if (stats.isDirectory()) {
       // read all files in dir
-      fs.readdirSync(pathToCurrentFile).map((file) => {
+      await Promise.all(fs.readdirSync(pathToCurrentFile).map(async (file) => {
         // append dir to filename and read file stats
         const pathToFile = `${pathToCurrentFile}\\${file}`;
 
-        const newFileName = createNewFileName(
+        const newFileName = await createNewFileName(
           file,
           pathToCurrentFile,
           fs.statSync(pathToFile),
         );
         console.log("newFileName:", newFileName);
 
-        rename(pathToFile, newFileName);
+        renameFile(pathToFile, newFileName);
         count++;
-      });
+      }));
     }
-  });
+  }));
   console.log(`Number of files changed ${count}`);
   console.timeEnd("startRenaming");
 };
 
-const createNewFileName = (file, pathToCurrentFile, stats) => {
+const getDateFromEXIF = async (file, pathToCurrentFile) => {
+  console.log(`${pathToCurrentFile}\\${file}`)
+  const file2 = fs.readFileSync(`${pathToCurrentFile}\\${file}`)
+  let { DateTimeOriginal } = await exifr.parse(file2);
+  return parseDateString(DateTimeOriginal);
+}
+
+const getDateFromLastModified = (stats) => {
+  return parseDateString(stats.mtime)
+}
+
+const parseDateString = (date) => {
+  const year = date.getFullYear();
+  // adding a leading 0, by slicing -2 we get the last 2 digits, so 020 becomes 20 and 02 stays 02.
+  // also months are 0 indexed
+  const month = leadingZero(date.getMonth() + 1);
+  const day = leadingZero(date.getDate());
+  const hours = leadingZero(date.getHours());
+  const minutes = leadingZero(date.getMinutes());
+  const seconds = leadingZero(date.getSeconds());
+
+  return [year, month, day, hours, minutes, seconds];
+}
+
+const getDateFromFileName = (file) => {
+  console.log(`${file} being reformated`);
+
+  const year = file.slice(4, 8);
+  // adding a leading 0, by slicing -2 we get the last 2 digits, so 020 becomes 20 and 02 stays 02.
+  // also months are 0 indexed
+  const month = file.slice(8, 10);
+  const day = file.slice(10, 12);
+  const hours = file.slice(13, 15);
+  const minutes = file.slice(15, 17);
+  const seconds = file.slice(17, 19);
+
+  return [year, month, day, hours, minutes, seconds];
+}
+
+const createNewFileName = async (file, pathToCurrentFile, stats) => {
   let year;
   let month;
   let day;
@@ -42,31 +82,14 @@ const createNewFileName = (file, pathToCurrentFile, stats) => {
   let minutes;
   let seconds;
 
-  if (!reformatFileName) {
-    const lastModified = stats.mtime;
-    console.log(`${file} Data Last Modified: ${lastModified}`);
-
-    // parse lastModified date to a new filename
-    year = lastModified.getFullYear();
-    // adding a leading 0, by slicing -2 we get the last 2 digits, so 020 becomes 20 and 02 stays 02.
-    // also months are 0 indexed
-    month = leadingZero(lastModified.getMonth() + 1);
-    day = leadingZero(lastModified.getDate());
-    hours = leadingZero(lastModified.getHours());
-    minutes = leadingZero(lastModified.getMinutes());
-    seconds = leadingZero(lastModified.getSeconds());
+  if (formatMethod === 'exif') {
+    [year, month, day, hours, minutes, seconds] = await getDateFromEXIF(file, pathToCurrentFile)
+  } else if (formatMethod == 'reformat') {
+    [year, month, day, hours, minutes, seconds] = getDateFromFileName(file);
   } else {
-    console.log(`${file} being reformated`);
-
-    year = file.slice(4, 8);
-    // adding a leading 0, by slicing -2 we get the last 2 digits, so 020 becomes 20 and 02 stays 02.
-    // also months are 0 indexed
-    month = file.slice(8, 10);
-    day = file.slice(10, 12);
-    hours = file.slice(13, 15);
-    minutes = file.slice(15, 17);
-    seconds = file.slice(17, 19);
+    [year, month, day, hours, minutes, seconds] = getDateFromLastModified(stats)
   }
+
 
   if (
     day.length < 2 ||
@@ -97,7 +120,7 @@ const leadingZero = (nbr) => {
   return "0".concat(nbr).slice(-2);
 };
 
-const rename = (oldPath, newPath) => {
+const renameFile = (oldPath, newPath) => {
   fs.renameSync(oldPath, newPath);
   console.log(`File renamed from ${oldPath} to ${newPath} successfully!`);
 };
